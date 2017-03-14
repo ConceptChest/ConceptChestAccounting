12f7e539d2fd59be21c3aeec5670cccf1e1b4f02
Concept Chest <conceptchest@gmail.com>
2017-03-14 23:15:16 +0500

    Ajax in progress
    For insert char of account

---
+++ ajax/ConceptChestAccountingCRUD.php
@@ -0,0 +1,81 @@
+
+<?php
+/*
+if (isset($_POST['title'])) {
+
+    $title = $_POST['title'];
+    $level = $_POST['level'];
+    $type = $_POST['type'];
+    $address = $_POST['address'];
+    $phone = $_POST['phone'];
+    $fax = $_POST['fax'];
+    $mobile = $_POST['mobile'];
+    $email = $_POST['email'];
+    $website = $_POST['website'];
+    $category = $_POST['category'];
+    $ntn = $_POST['ntn'];
+    $stn = $_POST['stn'];
+    $citycode = $_POST['citycode'];
+    $cityname = $_POST['cityname'];
+    $areacode = $_POST['areacode'];
+    $areaname = $_POST['areaname'];
+    $date = $_POST['date'];
+    
+  if($wpdb->insert( 
+	'wp_conceptchestaccounting_accounts', 
+	array( 
+		'TITLE' => $title, 
+		'LEVEL ' => $level,
+                'TYPE' => $type,
+                'Address' => $address,
+                'PH_NO' => $phone,
+                'FAX_NO' => $fax,
+                'MOB_NO' => $mobile,
+                'EMAIL' => $email,
+                'WEBSITE' => $website,
+                'CATEGORY' => $category,
+                'NTN' => $ntn,
+                'STN' => $stn,
+                'CITY_CODE' => $citycode,
+                'CITY_NAME' => $cityname,
+                'AREA_CODE' => $areacode,
+                'AREA_NAME' => $areaname,
+                'DATE' => $date
+            
+	), 
+	array( 
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s'
+		 
+	) 
+)===FALSE ){
+      
+      $msg = "Failed";
+      return $msg;
+}
+else{
+    $msg = "Sucess";
+    return $msg;
+    
+}
+    
+    
+}
+
+*/
+return "OK";
--- includes/ConceptChestCOAPannel.php
+++ includes/ConceptChestCOAPannel.php
@@ -1,73 +1,89 @@
+
+
 <div class="container form-style-5">
+    <script type="text/javascript"> 
+   
+		
+         
+       
+        jQuery("insert_form").click(function(){
+            var str = jQuery("#insert_form").serialize();	
+        jQuery.ajax({url: "<?phpplugins_url('ajax/ConceptChestAccountingCRUD.php', __FILE__) ?>",data: str, success: function(result){
+            jQuery("#note").html(result);
+          }});
+});
+   
+
+</script>
   <h2>Chart Of Account</h2>
-<form >
+  <form id="insert_form">
   <div class="form-group">
     <label for="title">Title</label>
-    <input type="text" class="form-control" id="title" placeholder="Title">
+    <input type="text" class="form-control" name="title" placeholder="Title">
   </div>
     <div class="form-group">
     <label for="level">Level</label>
-    <input type="text" class="form-control" id="level" placeholder="Level">
+    <input type="text" class="form-control" name="level" placeholder="Level">
   </div>
     <div class="form-group">
     <label for="type">Type</label>
-    <input type="text" class="form-control" id="type" placeholder="Type">
+    <input type="text" class="form-control" name="type" placeholder="Type">
   </div>
     <div class="form-group">
     <label for="address">Address</label>
-    <input type="text" class="form-control" id="address" placeholder="Address">
+    <input type="text" class="form-control" name="address" placeholder="Address">
   </div>
     <div class="form-group">
     <label for="type">Phone No.</label>
-    <input type="text" class="form-control" id="phone" placeholder="Phone No.">
+    <input type="text" class="form-control" name="phone" placeholder="Phone No.">
   </div>
     <div class="form-group">
     <label for="fax">Fax No.</label>
-    <input type="text" class="form-control" id="fax" placeholder="Fax No.">
+    <input type="text" class="form-control" name="fax" placeholder="Fax No.">
   </div>
     <div class="form-group">
     <label for="mobile">Mobile</label>
-    <input type="text" class="form-control" id="mobile" placeholder="Mobile">
+    <input type="text" class="form-control" name="mobile" placeholder="Mobile">
   </div>
     <div class="form-group">
     <label for="email">Email</label>
-    <input type="text" class="form-control" id="email" placeholder="Email">
+    <input type="text" class="form-control" name="email" placeholder="Email">
   </div>
     <div class="form-group">
     <label for="website">Website</label>
-    <input type="text" class="form-control" id="website" placeholder="Website">
+    <input type="text" class="form-control" name="website" placeholder="Website">
   </div>
     <div class="form-group">
     <label for="category">Category</label>
-    <input type="text" class="form-control" id="category" placeholder="Category">
+    <input type="text" class="form-control" name="category" placeholder="Category">
   </div>
     <div class="form-group">
     <label for="ntn">NTN</label>
-    <input type="text" class="form-control" id="ntn" placeholder="NTN">
+    <input type="text" class="form-control" name="ntn" placeholder="NTN">
   </div>
     <div class="form-group">
     <label for="stn">STN</label>
-    <input type="text" class="form-control" id="stn" placeholder="STN">
+    <input type="text" class="form-control" name="stn" placeholder="STN">
   </div>
     <div class="form-group">
     <label for="citycide">City Code</label>
-    <input type="text" class="form-control" id="citycode" placeholder="City Code">
+    <input type="text" class="form-control" name="citycode" placeholder="City Code">
   </div>
     <div class="form-group">
     <label for="cityname">City Name</label>
-    <input type="text" class="form-control" id="cityname" placeholder="City Name">
+    <input type="text" class="form-control" name="cityname" placeholder="City Name">
   </div>
     <div class="form-group">
     <label for="arecode">Area Code</label>
-    <input type="text" class="form-control" id="areacode" placeholder="Area Code">
+    <input type="text" class="form-control" name="areacode" placeholder="Area Code">
   </div>
     <div class="form-group">
     <label for="areaname">Area Name</label>
-    <input type="text" class="form-control" id="areaname" placeholder="Area Name">
+    <input type="text" class="form-control" name="areaname" placeholder="Area Name">
   </div>
   <div class="form-group">
     <label for="date">Date</label>
-    <input type="date"  class="form-control" id="date" placeholder="Date">
+    <input type="date"  class="form-control" name="date" placeholder="Date">
   </div>
     <div class="form-group">
         <p>
@@ -75,4 +91,7 @@
         </p>
     </div>
 </form>
+  <div id="#note"></div>
 </div>
+
+
