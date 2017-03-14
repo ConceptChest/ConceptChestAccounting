95253736133ead3910ffcce5ca7b1752d8193615
faizankhayani <faizan.khayani@hotmail.com>
2017-03-14 20:18:32 +0500

    coa
    form create

--- includes/ConceptChestCOAPannel.php
+++ includes/ConceptChestCOAPannel.php
@@ -1,11 +1,70 @@
 <form>
-  Title:<br>
-  <input type="text" name="title">
-  <br>
-  Amount:<br>
-  <input type="text" name="amount"><br>
-  Note:<br>
-  <input type="text" name="note">
-  <br>
-  <input type="button" name="submit" value="Submit">
+  <div class="form-group">
+    <label for="title">Title</label>
+    <input type="text" class="form-control" id="title" placeholder="Title">
+  </div>
+    <div class="form-group">
+    <label for="level">Level</label>
+    <input type="text" class="form-control" id="level" placeholder="Level">
+  </div>
+    <div class="form-group">
+    <label for="type">Type</label>
+    <input type="text" class="form-control" id="type" placeholder="Type">
+  </div>
+    <div class="form-group">
+    <label for="address">Address</label>
+    <input type="text" class="form-control" id="address" placeholder="Address">
+  </div>
+    <div class="form-group">
+    <label for="type">Phone No.</label>
+    <input type="text" class="form-control" id="phone" placeholder="Phone No.">
+  </div>
+    <div class="form-group">
+    <label for="fax">Fax No.</label>
+    <input type="text" class="form-control" id="fax" placeholder="Fax No.">
+  </div>
+    <div class="form-group">
+    <label for="mobile">Mobile</label>
+    <input type="text" class="form-control" id="mobile" placeholder="Mobile">
+  </div>
+    <div class="form-group">
+    <label for="email">Email</label>
+    <input type="text" class="form-control" id="email" placeholder="Email">
+  </div>
+    <div class="form-group">
+    <label for="website">Website</label>
+    <input type="text" class="form-control" id="website" placeholder="Website">
+  </div>
+    <div class="form-group">
+    <label for="category">Category</label>
+    <input type="text" class="form-control" id="category" placeholder="Category">
+  </div>
+    <div class="form-group">
+    <label for="ntn">NTN</label>
+    <input type="text" class="form-control" id="ntn" placeholder="NTN">
+  </div>
+    <div class="form-group">
+    <label for="stn">STN</label>
+    <input type="text" class="form-control" id="stn" placeholder="STN">
+  </div>
+    <div class="form-group">
+    <label for="citycide">City Code</label>
+    <input type="text" class="form-control" id="citycode" placeholder="City Code">
+  </div>
+    <div class="form-group">
+    <label for="cityname">City Name</label>
+    <input type="text" class="form-control" id="cityname" placeholder="City Name">
+  </div>
+    <div class="form-group">
+    <label for="arecode">Area Code</label>
+    <input type="text" class="form-control" id="areacode" placeholder="Area Code">
+  </div>
+    <div class="form-group">
+    <label for="areaname">Area Name</label>
+    <input type="text" class="form-control" id="areaname" placeholder="Area Name">
+  </div>
+    <div class="form-group">
+    <label for="date">Date</label>
+    <input type="date"  class="form-control" id="date" placeholder="Date">
+  </div>
 </form>
